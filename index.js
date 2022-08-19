5ca3693374d2d1954e15f42822b3c1dcdf8e7293
cakejune <tyq444@gmail.com>
2022-08-18 20:08:26 -0400

    commit tree 5bcd03f51078fdf25aa3bfdaa3f06fbf93fd74a6

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code here!
+
+
+function createElementAndId (tagName, idValue, elementContent)
+{
+    const element = document.createElement(tagName);
+    element.setAttribute("id", idValue);
+    element.textContent = elementContent;
+    //return element;
+}
+
+const main = document.getElementById("main");
+main.remove();
+const newHeader = createElementAndId("h1","victory", "Jake is the champion");
